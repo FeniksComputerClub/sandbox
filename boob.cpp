20d02949c40da69de50022b8fc8f3e60cea20dad
CarloFeniks <feniks.carlo@gmail.com>
2015-12-08 14:28:56 +0100

    Create boob.cpp
    A new-style Hello World proggie.

---
+++ boob.cpp
@@ -0,0 +1,11 @@
+// This is a new file!
+#include <iostream>   // Standard (C and C++) header have NO extention!
+                      // System (standard or other library) headers are included using < >,
+                      // local (project) headers usually using double quotes (" ").
+
+void main()           // Unlike C, main is allowed to return nothing in C++. This means we
+                      // can omit a 'return' statement at the end, but the netto result
+                      // will be as if we return 0.
+{
+  std::cout << "Hello boobs.\n";
+}
