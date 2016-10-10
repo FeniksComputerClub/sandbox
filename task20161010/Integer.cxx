2b878e26343ebe2aada0b180d594db0b576dc40d
David <none@gmail.com>
2016-10-10 13:53:24 +0200

    Add class Integer

---
+++ task20161010/Integer.cxx
@@ -0,0 +1,27 @@
+#include <Integer.h>
+#include <iostream>
+
+std::ostream& operator<<(std::ostream& os, Integer const& i)
+{
+  os << i.n();
+  return os;
+}
+
+Integer operator+(Integer const& i1, Integer const& i2)
+{
+  Integer sum;
+  sum.m_n = i1.m_n + i2.m_n;
+  return sum;
+}
+
+Integer operator*(Integer const& i1, Integer const& i2)
+{
+  Integer sum;
+  sum.m_n = i1.m_n * i2.m_n;
+  return sum;
+}
+
+bool operator==(Integer const& i1, Integer const& i2)
+{
+  return i1.m_n == i2.m_n;
+}
---
+++ task20161010/Integer.h
@@ -0,0 +1,49 @@
+#include <iostream>
+
+class Integer {
+  private:
+    int m_n;
+
+  public:
+
+  Integer() : m_n(0) {
+    std::cout << "Calling Integer::Integer()" << std::endl;
+  }
+  
+  Integer(int n) : m_n(n) {
+    std::cout << "Calling Integer::Integer(int)" << std::endl;
+  }
+
+  Integer(Integer const& i) : m_n(i.m_n) {
+    std::cout << "Calling Interger::Integer(Integer const&)" << std::endl;
+  }
+
+  Integer& operator=(Integer const& i){
+    m_n = i.m_n; 
+    std::cout << "Calling Interger::operator=(Integer const&)" << std::endl;
+  }
+
+  Integer operator~() const
+  {
+    Integer ret(~m_n);
+    return ret;
+  }
+
+  Integer operator-() const
+  {
+    Integer ret(-m_n);
+    return ret;
+  }
+
+  bool operator!() const
+  {
+    return !m_n;
+  }
+
+  friend Integer operator+(Integer const& i1, Integer const& i2);
+  friend Integer operator*(Integer const& i1, Integer const& i2);
+  friend bool operator==(Integer const& i1, Integer const& i2);
+
+  // Accessors.
+  int n() const { return m_n; }
+};
